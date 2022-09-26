d4c4dfda9ca3aa9df7e9c2e6f97162e6983317cd
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2022-09-27 01:48:04 +0400

    main docs update

--- src/index.tsx
+++ src/index.tsx
@@ -6,28 +6,23 @@ import App from './App';
 
 //TODO
 //========== Chalanging
-// Tree
-// Collapse
 // Popover
-// Segmented
 // Notification
 // Modal
 // Tabs
 // Popconfirm
 // Context menu (but not wrapper other)
-
-//========== Basic
 // Breadcrumb
 // DatePicker
 // ColorPicker
 // Slider
 // Avatar
-// Badge
 // Rate
-// Tag
 // Progress/Spin
 
 // add disabled to all component (plus pointer events none)
+
+// Hard
 // Smart Table
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
