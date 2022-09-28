3032839da794ad6f7d939ea663baa3a6a6466efd
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2022-09-29 01:08:58 +0400

    updated todo list

--- src/index.tsx
+++ src/index.tsx
@@ -5,23 +5,23 @@ import './index.scss';
 import App from './App';
 
 //TODO
-//========== Chalanging
-// Breadcrumb
+//========== Easy
 // DatePicker
 // ColorPicker
 // Slider
 // Avatar
 // Rate
 // Progress/Spin
+//===== Chalanging
 // Popconfirm
 // Context menu (but not wrapper other)
-
 // Notification
 // Modal
 
 // add disabled to all component (plus pointer events none)
 // autocomplete has some problems
 // tab needs mode functionality (remove, color change)
+// add scroll in listview
 
 // Hard
 // Smart Table
